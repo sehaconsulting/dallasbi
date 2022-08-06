d0994c537cc2b891725e713867a5dcc138be6157
ali.farid.rizwan@gmail.com <ali.farid.rizwan@gmail.com>
2022-08-06 22:26:33 +0000

    tst

--- models/stage/covid19_epic_ord.sql
+++ models/stage/covid19_epic_ord.sql
@@ -13,8 +13,8 @@ where a.order_code = 'covid'
 and b.order_ini = 'ORD'
 )
 
-select * , '2020-01-01' as LoadDatetime from a
+select * , getdate() as LoadDatetime from a
 {% if is_incremental() %}
-        where LastUpdateDate  > (select max(LastUpdateDate) as rd from  {{ this }} )     
+        where LastUpdateDate  > (select ifnull(max(LastUpdateDate), '1900-01-01')as rd from  {{ this }} )     
 {% endif %}
 
