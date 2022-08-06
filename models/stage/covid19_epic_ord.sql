2295ac4ee5f0666c37753c0e1919112f92ad2475
ali.farid.rizwan@gmail.com <ali.farid.rizwan@gmail.com>
2022-08-06 20:56:53 +0000

    fix code

--- models/stage/covid19_epic_ord.sql
+++ models/stage/covid19_epic_ord.sql
@@ -1,20 +1,17 @@
-{{ config(materialized='incremental'
-        , unique_key = 'order_id')
-             }}
-/*
-select 1 as userid, '2022-01-01 08:00:00' as EventTime, 'ord_placed' as EventType, '2022-01-03 11:00:00' as LoadDatetime
-union all
-select 2 as userid, '2022-01-02 08:00:00' as EventTime, 'ord_placed' as EventType, '2022-01-03 11:00:00' as LoadDatetime
-union all
-select 3 as userid, '2022-01-03 08:00:00' as EventTime, 'ord_placed' as EventType, '2022-01-03 11:00:00' as LoadDatetime
-*/
+{{
+    config(
+        materialized='incremental',
+        unique_key='order_id'
+    )
+}}
+
 
 select order_id, pt_id, order_date, getdate() as LoadDatetime 
 from {{ source('crsrc', 'epic_order') }} as a
 
-    {% if is_incremental() %}
+{% if is_incremental() %}
         inner join {{ source('crsrc', 'epic_order_cr_stat_alter') }} as b on a.order_id = b.order_id
         where b.update_date > (select max(LoadDatetime) as rd from  {{ this }} )
-    {% endif %}
+{% endif %}
 and a.order_code = 'covid'
 and b.order_ini = 'ORD'
