f4cb78ce9ee26f8e618735739d3db7877b83d0c1
ali.farid.rizwan@gmail.com <ali.farid.rizwan@gmail.com>
2022-08-07 03:24:23 +0000

    add event fact

--- models/analytics/covid19_epic_event_fact.sql
+++ models/analytics/covid19_epic_event_fact.sql
@@ -1,13 +1,15 @@
 {{ config(materialized='incremental') }}
 
-select userid, eventtime, eventtype, loaddatetime from "DEV"."TEAMA"."AUTH"
+select order_id, pt_id, order_date, lastupdatedate, 'ORD' as SourceType, getdate() as loaddatetime 
+from {{ref('covid19_epic_ord')}}
         {% if is_incremental() %}
-        where loaddatetime > (select max(LastRunDate) as rd from "DEV"."TEAMA"."RunHistory" where eventname = 'Auth' )
+        where lastupdatedate > (select ifnull(max(lastupdatedate), '1900-01-01' as rd from {{this}} where SourceType = 'ORD' )
         {% endif %}
 
     union all
 
-select userid, eventtime, eventtype, loaddatetime from "DEV"."TEAMA"."ORD"
+select diagnosis_id, pt_id, diagnosis_date, lastupdatedate, 'EDG' as SourceType, getdate() as loaddatetime 
+from {{ref('covid19_epic_diagnosis')}}
         {% if is_incremental() %}
-        where loaddatetime > (select max(LastRunDate) as rd from "DEV"."TEAMA"."RunHistory" where eventname = 'Ord' )
+        where lastupdatedate > (select ifnull(max(lastupdatedate), '1900-01-01' as rd from {{this}} where SourceType = 'EDG' )
         {% endif %}
