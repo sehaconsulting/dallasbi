1f10c68beef5ef2092b3a113c7c043d010e16f9d
ali.farid.rizwan@gmail.com <ali.farid.rizwan@gmail.com>
2022-08-07 01:30:54 +0000

    add sources diagnosis

--- models/stage/covid19_epic_enc_diag.sql
+++ models/stage/covid19_epic_enc_diag.sql
@@ -1,10 +1,19 @@
-{{ config(materialized='table') }}
-/*select 1 as userid, '2022-01-01 09:00:00' as EventTime, 'sign_complete' as EventType, '2022-01-03 10:00:00' as LoadDatetime
-union all
-select 2 as userid, '2022-01-02 09:00:00' as EventTime, 'sign_complete' as EventType, '2022-01-03 10:00:00' as LoadDatetime
-union all
-select 3 as userid, '2022-01-03 09:00:00' as EventTime, 'sign_complete' as EventType, '2022-01-03 10:00:00' as LoadDatetime
-*/
+{{
+    config(
+        materialized='incremental',
+        unique_key='diagnosis_id'
+    )
+}}
 
+with a as (
+select a.diagnosis_id, pt_id, diagnosis_date, b.update_date as LastUpdateDate 
+from {{ source('crsrc', 'epic_diagnosis') }} as a
+inner join {{ source('crsrc', 'epic_diagnosis_cr_stat_alter') }} as b on a.diagnosis_id = b.diagnosis_id
+where a.diagnosis_code = 'covid'
+and b.diagnosis_ini = 'EDG'
+)
 
-
+select * , getdate() as LoadDatetime from a
+{% if is_incremental() %}
+        where LastUpdateDate  > (select ifnull(max(LastUpdateDate), '1900-01-01')as rd from  {{ this }} )     
+{% endif %}
