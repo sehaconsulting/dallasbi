0dcd0f6c53974105412377c6cdf041694f5d850d
ali.farid.rizwan@gmail.com <ali.farid.rizwan@gmail.com>
2022-08-07 03:32:13 +0000

    add

--- models/analytics/covid19_epic_event_fact.sql
+++ models/analytics/covid19_epic_event_fact.sql
@@ -1,15 +1,15 @@
 {{ config(materialized='incremental') }}
 
-select order_id, pt_id, order_date, lastupdatedate, 'ORD' as SourceType, getdate() as loaddatetime 
+select order_id as event_id, pt_id, order_date as event_date, lastupdatedate, 'ORD' as event_type, getdate() as loaddatetime 
 from {{ref('covid19_epic_ord')}}
         {% if is_incremental() %}
-        where lastupdatedate > (select ifnull(max(lastupdatedate), '1900-01-01' as rd from {{this}} where SourceType = 'ORD' )
+        where lastupdatedate > (select ifnull(max(lastupdatedate), '1900-01-01') as rd from {{this}} where SourceType = 'ORD' )
         {% endif %}
 
     union all
 
-select diagnosis_id, pt_id, diagnosis_date, lastupdatedate, 'EDG' as SourceType, getdate() as loaddatetime 
+select diagnosis_id  as event_id, pt_id, diagnosis_date as event_date, lastupdatedate, 'EDG' as event_type, getdate() as loaddatetime 
 from {{ref('covid19_epic_diagnosis')}}
         {% if is_incremental() %}
-        where lastupdatedate > (select ifnull(max(lastupdatedate), '1900-01-01' as rd from {{this}} where SourceType = 'EDG' )
+        where lastupdatedate > (select ifnull(max(lastupdatedate), '1900-01-01') as rd from {{this}} where SourceType = 'EDG' )
         {% endif %}
