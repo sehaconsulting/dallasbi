31a1194a6b99c0fc0f125e6cd7d2a67f2c18e617
ali.farid.rizwan@gmail.com <ali.farid.rizwan@gmail.com>
2022-08-11 12:53:07 +0000

    check target scope

--- models/stage/test_tag.sql
+++ models/stage/test_tag.sql
@@ -1,10 +1,5 @@
+{{ config(materialized='table') }}
 
-{{
-
-    config(
-        tags = ["covid"]
-    )
-}} 
 select a.order_id, pt_id, order_date, b.update_date as LastUpdateDate, getdate() as LoadDatetime
 from {{ source('crsrc', 'epic_order') }} as a
 inner join {{ source('crsrc', 'epic_order_cr_stat_alter') }} as b on a.order_id = b.order_id 
