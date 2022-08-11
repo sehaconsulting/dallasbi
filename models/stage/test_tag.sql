f067e8a2d6c9b5396513e62812866450226a404c
ali.farid.rizwan@gmail.com <ali.farid.rizwan@gmail.com>
2022-08-11 12:50:24 +0000

    add getdate

--- models/stage/test_tag.sql
+++ models/stage/test_tag.sql
@@ -5,7 +5,7 @@
         tags = ["covid"]
     )
 }} 
-select a.order_id, pt_id, order_date, b.update_date as LastUpdateDate 
+select a.order_id, pt_id, order_date, b.update_date as LastUpdateDate, getdate() as LoadDatetime
 from {{ source('crsrc', 'epic_order') }} as a
 inner join {{ source('crsrc', 'epic_order_cr_stat_alter') }} as b on a.order_id = b.order_id 
 where a.order_code 
