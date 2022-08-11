64a4d815ba54c8b67576fb9c54e155226e56dae4
ali.farid.rizwan@gmail.com <ali.farid.rizwan@gmail.com>
2022-08-11 12:44:12 +0000

    add tags

---
+++ models/stage/test_tag.sql
@@ -0,0 +1,21 @@
+
+{{
+
+    config(
+        tags = ["covid"]
+    )
+}} 
+select a.order_id, pt_id, order_date, b.update_date as LastUpdateDate 
+from {{ source('crsrc', 'epic_order') }} as a
+inner join {{ source('crsrc', 'epic_order_cr_stat_alter') }} as b on a.order_id = b.order_id 
+where a.order_code 
+    {%- if target.name == 'covid'  -%}
+
+     
+      = 'covid'
+
+    {%- else -%}
+	 
+      <> 'covid'
+
+    {%- endif -%}
